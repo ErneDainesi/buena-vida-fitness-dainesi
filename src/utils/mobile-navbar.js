fbb6cb60628b3fd44e4c9d2c8f0f90bbc695ee87
ErneDainesi <ernestodainesiperrot@gmail.com>
2021-06-02 15:40:33 -0300

    arreglo typo

--- src/utils/mobile-navbar.js
+++ src/utils/mobile-navbar.js
@@ -4,7 +4,7 @@ const navLinks = document.querySelector("#mobile-menu-links");
 
 navMenu.addEventListener("click", (e) => {
 	if (e.target.id === navMenu.id) {
-		if (nav.classList.contains("showMenu")) {
+		if (navLinks.classList.contains("showMenu")) {
 			navLinks.classList.remove("showMenu");
 		} else {
 			navLinks.classList.add("showMenu");
