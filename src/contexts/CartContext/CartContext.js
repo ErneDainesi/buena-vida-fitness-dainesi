e190380df2e01f9eb00dad84fe3f01ab70aec983
ErneDainesi <ernestodainesiperrot@gmail.com>
2021-07-01 17:54:41 -0300

    arreglo bugs

--- src/contexts/CartContext/CartContext.js
+++ src/contexts/CartContext/CartContext.js
@@ -5,19 +5,14 @@ export const CartContext = createContext();
 export const CartProvider = props => {
 	const [cart, setCart] = useState([]);
 	const isInCart = itemId => {
-		for (const product of cart) {
-			if (product.item.id === itemId) {
-				return true;
-			}
-		}
-		return false;
-	};
+		return cart.findIndex(item => item.item.id === itemId) >= 0;
+	}
 	const updateCart = (item, quantity) => {
 		const selectedItem = cart.find(product => product.item.id === item.id);
 		const newQuantity = selectedItem.quantity + quantity;
 		if (newQuantity <= selectedItem.item.stock) {
 			selectedItem.quantity += quantity;
-			console.log("Updated Cart", cart);
+			setCart([...cart]);
 		} else {
 			console.error("Selected amount exceeds current stock");
 		}
@@ -28,10 +23,28 @@ export const CartProvider = props => {
 	const emptyCart = () => {
 		setCart([]);
 	};
+	const itemsInsideCart = () => {
+		return cart;
+	}
+	const removeFromCart = itemId => {
+		const indexOfItem = cart.findIndex(item => item.item.id === itemId);
+		cart.splice(indexOfItem, 1);
+		setCart([...cart]);
+	}
+	const isCartEmpty = () => {
+		return cart.length === 0;
+	}
+	const cartItemQuantity = () => {
+		let count = 0;
+		for (const item of cart) {
+			count += item.quantity;
+		}
+		return count;
+	}
 	useEffect(() => {
 		console.log("Updated Cart", cart);
 	});
-	return <CartContext.Provider value={{addToCart, emptyCart, itemsInsideCart, removeFromCart}}>
+	return <CartContext.Provider value={{addToCart, emptyCart}}>
 		{props.children}
 	</CartContext.Provider>
 }
