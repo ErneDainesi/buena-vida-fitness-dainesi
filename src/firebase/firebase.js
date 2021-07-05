b146ea4893dddc60b0b951b090cf758f5f58dbbf
ErneDainesi <ernestodainesiperrot@gmail.com>
2021-07-05 11:14:59 -0300

    agrego carpeta de firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/storage';
+
+const firebaseConfig = {
+	apiKey: "AIzaSyABrXHak_V26f7fLRxtibiJ0mG58RZsfYE",
+	authDomain: "buena-vida-fitness.firebaseapp.com",
+	projectId: "buena-vida-fitness",
+	storageBucket: "buena-vida-fitness.appspot.com",
+	messagingSenderId: "645943436692",
+	appId: "1:645943436692:web:952ec1a1611b1350065b81"
+};
+
+const fb = firebase.initializeApp(firebaseConfig);
+
+export const database = fb.firestore();
+export const storage = fb.storage();
+
