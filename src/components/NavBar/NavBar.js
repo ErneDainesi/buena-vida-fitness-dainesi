67bf391e4f7b10ff8f94dadb7bde0c4b2114678d
erne <edainesi@fi.uba.ar>
2021-07-11 20:51:55 -0300

    trabajo el responsive

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,13 @@
-import React, {useContext} from 'react';
+import React, {useState, useContext} from 'react';
 import Logo from '../../images/logo-buena-vida-circle.png'
 import {CartWidget} from './components/CartWidget/CartWidget';
 import {Link} from 'react-router-dom';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faCaretDown} from '@fortawesome/free-solid-svg-icons';
+import {faCaretDown, faBars} from '@fortawesome/free-solid-svg-icons';
 import {CartContext} from '../../contexts/CartContext/CartContext';
 
 export const NavBar = () => {
+	const [toggleMenu, setToggleMenu] = useState(false);
 	const {isCartEmpty} = useContext(CartContext);
 	return <>
 		<nav className="desktop-nav">
@@ -15,24 +16,27 @@ export const NavBar = () => {
 					<img src={Logo} alt="logo" className="logo-img" />
 				</Link>
 				<h1 className="nav-title">Buena Vida Fitness</h1>
+				<div className="mobile-menu" onClick={() => setToggleMenu(!toggleMenu)}><FontAwesomeIcon icon={faBars} /></div>
 			</div>
-			<div className="nav-links">
-				<Link className="nav-link" to="/">Inicio</Link>
-				<div className="dropdown">
-					<Link className="nav-link" to="/productos/todo">
-						Productos
-						<FontAwesomeIcon icon={faCaretDown} className="dropdown-icon" />
-					</Link>
-					<div className="dropdown-content">
-						<Link className="dropdown-link" to="/productos/frutos-secos">Frutos Secos</Link>
-						<Link className="dropdown-link" to="/productos/frutos-rojos">Frutos Rojos</Link>
-						<Link className="dropdown-link" to="/productos/cereales">Cereales</Link>
-						<Link className="dropdown-link" to="/productos/semillas">Semillas</Link>
+			<div className={`mobile-nav ${toggleMenu ? "show-menu" : ""}`}>
+				<div className="nav-links">
+					<Link className="nav-link" to="/" onClick={() => setToggleMenu(false)}>Inicio</Link>
+					<div className="dropdown">
+						<Link className="nav-link" to="/productos/todo" onClick={() => setToggleMenu(false)}>
+							Productos
+							<FontAwesomeIcon icon={faCaretDown} className="dropdown-icon" />
+						</Link>
+						<div className="dropdown-content">
+							<Link className="dropdown-link" to="/productos/frutos-secos">Frutos Secos</Link>
+							<Link className="dropdown-link" to="/productos/frutos-rojos">Frutos Rojos</Link>
+							<Link className="dropdown-link" to="/productos/cereales">Cereales</Link>
+							<Link className="dropdown-link" to="/productos/semillas">Semillas</Link>
+						</div>
 					</div>
+					<Link className="nav-link" to="/nosotros" onClick={() => setToggleMenu(false)}>Nosotros</Link>
+					<Link className="nav-link" to="/contacto" onClick={() => setToggleMenu(false)}>Contacto</Link>
+					{!isCartEmpty() && <CartWidget />}
 				</div>
-				<Link className="nav-link" to="/nosotros">Nosotros</Link>
-				<Link className="nav-link" to="/contacto">Contacto</Link>
-				{!isCartEmpty() && <CartWidget />}
 			</div>
 		</nav>
 	</>;
